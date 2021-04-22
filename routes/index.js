06f8f33adf415bafb7c59634180b25f453365a32
Olivia Leggio <o.r.legg12@gmail.com>
2021-04-21 20:05:29 -0400

    added all items, edits to category link styling

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 let MongoClient = require('mongodb').MongoClient;
 let uri = "mongodb+srv://testuser:12345@cluster0.prj3a.mongodb.net/leggio_dough?retryWrites=true&w=majority";
 //const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+/*
 MongoClient.connect(uri, {useUnifiedTopology: true}, (err, client) => {
   if (err) return console.error(err)
     console.log('Connected to db');
@@ -19,18 +19,13 @@ MongoClient.connect(uri, {useUnifiedTopology: true}, (err, client) => {
       }).catch(error => console.log(error));
     })
 });
-
+*/
 
 
 
 //working section with hard coded data
-/*
 
 router.get('/', (req, res) => {
-  res.render('home');
-});
-
-router.get('/products', (req, res) => {
   res.render('products', 
   {"products": [
     {"name": "Tortellini", 
@@ -43,8 +38,68 @@ router.get('/products', (req, res) => {
      "quantity": 3,
      "price": 3.00,
      "description": "ldalleiadfkalkf",
+     "category": "pasta"},
+
+    {"name": "Fusilli",
+     "quantity": 3,
+     "price": 3.00,
+     "description": "ldalleiadfkalkf",
+     "category": "pasta"},
+    
+    {"name": "Garganelli",
+     "quantity": 3,
+     "price": 3.00,
+     "description": "ldalleiadfkalkf",
+     "category": "pasta"},
+
+    {"name": "Ravioli",
+     "quantity": 3,
+     "price": 3.00,
+     "description": "ldalleiadfkalkf",
+     "category": "pasta"},
+    
+    {"name": "Sacchetti",
+     "quantity": 3,
+     "price": 3.00,
+     "description": "ldalleiadfkalkf",
      "category": "pasta"}],
   "category": "pasta"
+  });
+});
+
+router.get('/bread', (req, res) => {
+  res.render('products', 
+  {"products": [
+    {"name": "Baguette",
+     "quantity": 2,
+     "price": 3.00,
+     "description": "alaewkefjeiao",
+     "category": "bread"}],
+   "category": "bread"
+  });
+});
+
+router.get('/pastries', (req, res) => {
+  res.render('products', 
+  {"products": [
+    {"name": "Croissant",
+     "quantity": 6,
+     "price": 2.00,
+     "description": "alaewkefjeiao",
+     "category": "pastries"},
+
+    {"name": "Pain au Chocolat",
+     "quantity": 6,
+     "price": 2.00,
+     "description": "alaewkefjeiao",
+     "category": "pastries"},
+    
+    {"name": "Macarons",
+     "quantity": 5,
+     "price": 5.00,
+     "description": "alaewkefjeiao",
+     "category": "pastries"}],
+   "category": "pastries"
   });
 });
 
@@ -55,5 +110,5 @@ router.get('/about', (req, res) => {
 router.get('/cart', (req, res) => {
   res.render('cart');
 });
-*/
+
 module.exports = router;
